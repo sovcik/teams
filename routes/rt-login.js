b553c1b715990d74b7330aa5f1e427283f410b37
Jozef Sovcik <jozef@sovcik.net>
2017-06-02 16:33:06 +0200

    after login open profile

--- routes/rt-login.js
+++ routes/rt-login.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
 
 router.post('/',
     auth.passport.authenticate('local', {
-        successReturnToOrRedirect: '/',
+        successReturnToOrRedirect: '/profile',
         failureRedirect: '/login',
         failureFlash: true
     })
