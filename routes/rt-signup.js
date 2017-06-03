168b02564e1f293a1b604744ed136c9ee0658edf
Jozef Sovcik <jozef@sovcik.net>
2017-06-03 09:04:23 +0200

    Implementing basic signup

--- routes/rt-signup.js
+++ routes/rt-signup.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const auth = require('../lib/auth.js');
 const router = express.Router();
+
+const User = mongoose.models.User;
 
 module.exports = router;
 
@@ -13,15 +16,35 @@ router.get('/', function (req, res, next) {
         res.render('signup');
 });
 
-router.post('/', function (req, res, next) {
-    const uv = new mongoose.model('UserVerify');
-    const username = req.user.username;
-    uv.addNew(dbConnection, username, req.user.username, req.user.password, function(err, user){
+router.post('/', async function (req, res, next) {
+    //const uv = new mongoose.model('UserVerify');
+    //const username = req.body.email;
+    console.log(req);
+
+    try {
+        const s = await bcrypt.genSalt(1);
+        const h = await bcrypt.hash(req.body.password, s);
+        const user = await User.create(
+            {
+                username: req.body.email,
+                passwordHash: h,
+                salt: s,
+                fullName: req.body.email,
+                email: req.body.email
+            });
+        console.log("User created: " + user.username + "===" + user.id);
+        res.redirect('/profile');
+    } catch (err) {
+        res.render('error', {message:"Nepodarilo sa vytvoriť účet", error:err});
+    }
+    /*
+    u = await uv.addNew(dbConnection, username, req.user.username, req.user.password, function(err, user){
         if (err) {
             console.log("Error creating signup record for "+username);
             return;
         }
         console.log("Signup created for "+username);
     });
+    */
 
 });
