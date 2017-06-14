d20261a92e2e2c7289d7e5bcba4ae93f0a29f35d
Jozef Sovcik <jozef@sovcik.net>
2017-06-14 10:45:55 +0200

    password validation for signup implemented #1

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -16,3 +16,16 @@ function urlGetParam(param) {
 function date2ISOString(datetime){
     return datetime.getFullYear()+'-'+datetime.getMonth()+'-'+datetime.getDate();
 }
+
+function validateEmail(email){
+    console.log("Validating email");
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(email);
+}
+
+function validatePassword(pwd){
+    // at has to contain number and small letter and has to be at least 6 chars long
+    console.log("Validating password");
+    const re = /(?=.*[0-9])(?=.*[a-z]).{6,}/;
+    return re.test(pwd);
+}
---
+++ public/javascripts/signup.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function initSignup(){
+    console.log("/signup - Initializing");
+
+    $('#frmSignup').on("submit",validateSignupForm);
+
+    console.log("/signup - Initializing completed");
+}
+
+function validateSignupForm(){
+    console.log("Validating form");
+    if ($('#userName').val().trim().length == 0) {
+        $('#userNameGrp').addClass("has-error");
+        alert("Prihlasovacie meno nesmie byť prázdne.");
+        return false;
+    } else
+        $('#userNameGrp').removeClass("has-error");
+
+    if ($('#fullName').val().trim().length == 0) {
+        $('#fullNameGrp').addClass("has-error");
+        alert("Meno a priezvisko nesmie byť prázdne.");
+        return false;
+    } else
+        $('#fullNameGrp').removeClass("has-error");
+
+    if (!validatePassword($('#password').val())) {
+        $('#passwordGrp').addClass("has-error");
+        alert("Heslo musí obsahovať aspoň jedno číslo, malé písmeno a musí mať aspoň 6 znakov.");
+        return false;
+    } else
+        $('#fullNameGrp').removeClass("has-error");
+
+
+    console.log("Form OK");
+    return true; // return false to cancel form action
+}
+
