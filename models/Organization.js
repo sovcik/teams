a3fdae4daad0a813f14cfb0638f5c201a45f1aec
Jozef Sovcik <jozef@sovcik.net>
2017-06-25 14:04:07 +0200

    adding bank account to organization

--- models/Organization.js
+++ models/Organization.js
@@ -7,6 +7,8 @@ const OrganizationSchema = new mongoose.Schema({
     companyNo: { type: String },
     taxNo: { type: String },
     VATNo: { type: String },
+    bankAccount: {type: String},
+    bankSWIFT: {type: String}
 });
 
 OrganizationSchema.testData = function(rec, prefix){
@@ -14,6 +16,8 @@ OrganizationSchema.testData = function(rec, prefix){
     rec.companyNo = prefix + "CNo";
     rec.taxNo = prefix + "TxNo";
     rec.VATNo = prefix + "VATNo";
+    rec.bankAccount = "SK26 1234 1234 1234 1234" + prefix;
+    rec.bankSWIFT = "SWIFTXYZ";
 
     return rec;
 };
