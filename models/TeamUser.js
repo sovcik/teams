0ce8b7f8700ac7830d5d11aa386c093ce0055759
Jozef Sovcik <jozef@sovcik.net>
2017-09-03 20:41:11 +0200

    removing status plugin from TeamUser

--- models/TeamUser.js
+++ models/TeamUser.js
@@ -3,15 +3,12 @@
  */
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-const statusPlugin = require('./plugins/status-plugin');
 
 const TeamUserSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     teamId: { type: String, required: true },
     role: { type: String, required: true }  // coach, member
 });
-
-TeamUserSchema.plugin(statusPlugin);
 
 if (!mongoose.models.TeamUser) {
     mongoose.model('TeamUser', TeamUserSchema);
