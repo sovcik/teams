96f4157683a538e805bbf0e00be4ef87fa5d75f0
jozef <jsdev2015@gmail.com>
2018-12-28 22:30:48 +0100

    replacing bcrypt with bcryptjs

--- routes/rt-signup.js
+++ routes/rt-signup.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const auth = require('../lib/auth.js');
 const router = express.Router();
