c9861e00ed0b0c2122ac75e3340c691c795039f5
Jozef Sovcik <jozef@sovcik.net>
2020-07-14 13:48:44 +0200

    adding system version

--- routes/rt-login.js
+++ routes/rt-login.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../lib/auth.js');
+const version = require('../package.json').version;
 
 module.exports = router;
 
 router.get('/', function(req, res, next) {
     const success = req.query.success;
-    res.render('login', { success: success });
+    res.render('login', { success: success, version: version });
 });
 
 router.post(
