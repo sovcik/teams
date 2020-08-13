e80eecd460f9e9ff12b38bacb0f97fcb89f61957
Jozef Sovcik <jozef@sovcik.net>
2020-08-13 13:58:32 +0200

    libs upgraded

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function startAll(res) {
     app.set('view engine', 'pug');
 
     app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-    app.use(logger('dev'));
+    app.use(logger('tiny'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
